4214675b186aaac20fc281cb29b4ee594029d35e
Ba7athproject <ba7ath.project@gmail.com>
2026-02-09 19:29:35 +0000

    Fix fetch errors from frontend to login API

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Noto Sans Arabic"', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
